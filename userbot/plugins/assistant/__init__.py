a1cb94e876537f422b45fd3c43ba087c1fd67c32
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-03-22 09:48:34 -0700

    Update __init__.py

--- userbot/plugins/assistant/__init__.py
+++ userbot/plugins/assistant/__init__.py
@@ -1,4 +1,4 @@
-# LegendBot Assistant
+# Invisible Legend XBot Assistant
 from telethon import Button, custom
 
 from userbot import ALIVE_NAME, bot
@@ -10,9 +10,9 @@ OWNER_ID = bot.uid
 
 
 LEGEND_USER = bot.me.first_name
-Its_LegendBoy = bot.uid
+MR_INVISIBLE_OFFICIAL = bot.uid
 
-legend_mention = f"[{LEGEND_USER}](tg://user?id={Its_LegendBoy})"
+legend_mention = f"[{LEGEND_USER}](tg://user?id={MR_INVISIBLE_OFFICIAL})"
 gban_pic = "./userbot/resources/pics/gban.mp4"
 main_pic = "./userbot/resources/pics/main.jpg"
 core_pic = "./userbot/resources/pics/core.jpg"
@@ -20,12 +20,12 @@ chup_pic = "./userbot/resources/pics/chup.mp4"
 bsdk_pic = "./userbot/resources/pics/bsdk.jpg"
 bsdkwale_pic = "./userbot/resources/pics/bsdk_wale.jpg"
 chutiya_pic = "./userbot/resources/pics/chutiya.jpg"
-LEGENDversion = "3.0"
+LEGENDversion = "1.0"
 
-perf = "[ Pro Lêɠêɳ̃dẞø† ]"
+perf = "[ Invisible Legendẞø† ]"
 
 
-DEVLIST = ["5080268903"]
+DEVLIST = ["5267090352"]
 
 
 async def setit(event, name, value):
