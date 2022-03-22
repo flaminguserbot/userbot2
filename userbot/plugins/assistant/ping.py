b916429ac3408759dc27768b49ec21451c3c0746
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-03-22 10:00:03 -0700

    Update ping.py

--- userbot/plugins/assistant/ping.py
+++ userbot/plugins/assistant/ping.py
@@ -7,15 +7,15 @@ from userbot.Config import Config
 from userbot.plugins import *
 
 LEGEND_IMG = os.environ.get(
-    "BOT_PING_PIC", "https://telegra.ph/file/a9f6a3c160977352dd595.jpg"
+    "BOT_PING_PIC", "https://te.legra.ph/file/8b012f55fc4238151d169.jpg"
 )
 ms = 4
 ALIVE = Config.ALIVE_NAME
 
-LegendBoy = f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE}』"
+MR_INVISIBLE_OFFICIAL = f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE}』"
 
 
 @tgbot.on(events.NewMessage(pattern="^/ping"))
 async def _(event):
-    GOOD = [[Button.url("⚜ Lêɠêɳ̃dẞø† ⚜", "https://t.me/LegendBot_Pros")]]
-    await tgbot.send_file(event.chat_id, LEGEND_IMG, caption=LegendBoy, buttons=GOOD)
+    GOOD = [[Button.url("⚜ Invisible Legends ⚜", "https://t.me/Invisible_LegendBot")]]
+    await tgbot.send_file(event.chat_id, LEGEND_IMG, caption=MR_INVISIBLE_OFFICIAL, buttons=GOOD)
