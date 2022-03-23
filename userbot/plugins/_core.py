888f80f00ea3e749cc1b76462d018dbcb539259e
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-03-23 07:27:50 -0700

    Update _core.py

--- userbot/plugins/_core.py
+++ userbot/plugins/_core.py
@@ -10,7 +10,7 @@ from userbot.utils import *
 from . import *
 
 DELETE_TIMEOUT = 5
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "『Lêɠêɳ̃dẞø†』"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "『ɪɴᴠɪ🅢ɪʙʟᴇ Bot』"
 legend = bot.uid
 LEGEND = f"[{DEFAULTUSER}](tg://user?id={legend})"
 EVAL = os.environ.get("EVAL", None)
@@ -24,7 +24,7 @@ async def send(event):
     message_id = event.message.id
     thumb = core_pic
     input_str = event.pattern_match.group(1)
-    omk = f"**⍟ 𝙿𝚕𝚞𝚐𝚒𝚗 𝚗𝚊𝚖𝚎 ≈** `{input_str}`\n**⍟ 𝚄𝚙𝚕𝚘𝚊𝚍𝚎𝚍 𝙱𝚢 ≈** {legend_mention}\n\n⚜ **[Pro-Lêɠêɳ̃dẞø†](https://t.me/LegendBot_Pros)** ⚜"
+    omk = f"**⍟ 𝙿𝚕𝚞𝚐𝚒𝚗 𝚗𝚊𝚖𝚎 ≈** `{input_str}`\n**⍟ 𝚄𝚙𝚕𝚘𝚊𝚍𝚎𝚍 𝙱𝚢 ≈** {legend_mention}\n\n⚜ **[ɪɴᴠɪ🅢ɪʙʟᴇẞø†](https://t.me/Invisible_LegendBot)** ⚜"
     the_plugin_file = "./userbot/plugins/{}.py".format(input_str)
     the_1plugin_file = "./userbot/plugins/Abuse/{}.py".format(input_str)
     the_2plugin_file = "./userbot/plugins/Spam/{}.py".format(input_str)
@@ -128,7 +128,7 @@ async def install(event):
                                     b = 1
                                 await legend.edit(a)
                             return await legend.edit(
-                                f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {legend_mention}\n\n{string}\n\n        ⚡ **[Pro-Lêɠêɳ̃dẞø†]({chnl_link})** ⚡",
+                                f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {legend_mention}\n\n{string}\n\n        ⚡ **[ɪɴᴠɪ🅢ɪʙʟᴇẞø†]({chnl_link})** ⚡",
                                 link_preview=False,
                             )
 
@@ -219,7 +219,7 @@ async def install(event):
     o = stdout.decode()
     _o = o.split("\n")
     o = "\n".join(_o)
-    OUTPUT = f"♥️List Of Plugins In 𝖑𝖊ɠêɳ̃dẞø✞︎ 🇮🇳 :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/LegendBot_Pros for help."
+    OUTPUT = f"♥️List Of Plugins In ɪɴᴠɪ🅢ɪʙʟᴇẞø✞︎ 🇮🇳 :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/LegendBot_Pros for help."
     if len(OUTPUT) > 69:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
             out_file.name = "cmnds_list.text"
@@ -233,7 +233,7 @@ async def install(event):
             )
             await edit_or_reply(
                 LEGEND_file,
-                f"**Output Too Large. This is the file for the list of plugins in L𝖊ɠêɳ̃dẞø✞︎**.\n\nBY :- **{DEFAULTUSER}**",
+                f"**Output Too Large. This is the file for the list of plugins in ɪɴᴠɪ🅢ɪʙʟᴇẞø✞︎**.\n\nBY :- **{DEFAULTUSER}**",
             )
             await event.delete()
 
@@ -257,7 +257,7 @@ CmdHelp("core").add_command(
     "Sends the given file from your userbot server, if any.",
     "send alive",
 ).add_command(
-    "cmds", None, "Gives out the list of modules in LegendBot."
+    "cmds", None, "Gives out the list of modules in ɪɴᴠɪ🅢ɪʙʟᴇBot."
 ).add_warning(
     "Before Install Any Plugin.Open That File file by command .open"
 ).add_info(
