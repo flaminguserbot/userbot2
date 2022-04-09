8c49565144d8a7b7f51ce38b77a2b770291a275b
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-04-09 09:20:39 -0700

    Create LegendBot-v2.0.py

---
+++ LegendBot-V2.0/LegendBot-v2.0.py
@@ -0,0 +1,39 @@
+input java script system 
+input system in await(system_v)))
+ legendbot-v2.0 = await : LegendBot)()
+    LegendBot = Legend_User{Os}improt?
+    
+    import* 
+    javascript = V2.0
+    print(V2.0).send_messege._os await import 
+    java 
+    php, 
+    linux, 
+    shell, 
+    python, 
+    hack,
+    doker,
+    profilec, 
+    )
+     
+      
+       
+         
+            await : send_messege(1001639577866).get
+                 get_event(event:)
+                      send = sessoin_save()
+                               save_send_messege(save()())))
+                    .get.event.hack = java, python , linux, 
+                    ]
+                    
+                    
+                     [import hacking sof.get os in 
+                      hack= not/truv
+                      not/falsh,
+                      send,get,hack,otp,pass,
+                      
+                      
+ "ALIVE MODIOLS JAVA "
+  "ALIVE MODE ON BYE LEGENDX-PRO"
+ "ALIVE LEGENDBOT-V2.0"
+                    
