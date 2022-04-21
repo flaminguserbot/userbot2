e75ea4b16500b7800bae404da2cdfa15534efc44
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-04-21 08:41:02 -0700

    Create root-Userbot.js

---
+++ root-Userbot.js
@@ -0,0 +1,13 @@
+from import* java
+from await : python,linux
+from ..utils.os.clinet
+ 
+
+     LegendBot = mirror_mod_security = 1
+       mirror_mod_security = 1
+  CYBER__SECURITY = False,
+    none, ("legendbot_cyber")).get
+  Legend_messege:("989765756")).sleep(s%5)))}
+  sleep_time{(*nonetypes,)}.os_event:
+print("os").import*
+  "CYBER PROTECTING BY LEGEND X PRO SECURITY"
